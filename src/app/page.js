96a6cf0892cf595e813dc83754147c44c3f3dd61
voydd <deandrerandolph441@gmail.com>
2024-07-06 08:08:18 -0400

    letter change

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function page(){
             <h1 className=' text-sm'> The function will be held on <span className=' underline'>July 27th 2024 from 6-9pm.</span></h1>
             <h1 className='text-sm'>location: <a href="https://www.google.com/maps/place/1083+Austin+Ave+NE,+Atlanta,+GA+30307/@33.7619716,-84.3550701,17z/data=!3m1!4b1!4m6!3m5!1s0x88f506aa2471172b:0x80cd3712f280919e!8m2!3d33.7619716!4d-84.3524952!16s%2Fg%2F11bw3xnj77?entry=ttu" className='underline'>1083 Austin Ave NE, Atlanta, GA 30307</a></h1>
             <h3 className='text-sm '>Dress code: dress to impress or business casual, children do not have a dress code.</h3>
-            <h3 className='text-sm font-bold'>Please Be On Time. Something Special Is Planned and You will Not Want To Miss It.</h3>
+            <h3 className='text-sm font-bold'>Please Be On Time. Something Special Is Planned and You Will Not Want To Miss It.</h3>
 
             <h1 className='text-3xl my-6'>RSVP Here:</h1>
            
